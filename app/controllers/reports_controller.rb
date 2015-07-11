1a445d38cb4dfbd0464f05c0ce7ab748987dc53e
sim1hill <simone.c.hill@gmail.com>
2015-07-11 16:55:20 -0700

    geolocation for schools working

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -20,7 +20,8 @@ class ReportsController < ApplicationController
 
 
   def get_geo
-    @report = Report.create
+  schools = School.within(5, :origin => [params[:lat], params[:lng]])
+  binding.pry
     respond_to do |format|
       format.json 
     end
